83b25fa375b22a33bb324366445217167873b823
marcos_daniel <danielsinho487@gmail.com>
2023-06-13 09:06:09 -0300

    at

--- app/src/main/java/br/com/ifsuldeminas/cronometro/MainActivity.java
+++ app/src/main/java/br/com/ifsuldeminas/cronometro/MainActivity.java
@@ -1,18 +1,25 @@
 package br.com.ifsuldeminas.cronometro;
 
+import static android.text.format.DateUtils.formatElapsedTime;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvElapsedTime;
     private Button btnStartPause;
     private Button btnReset;
+    private Button btnSave;
     private boolean isRunning;
     private long startTime;
     private long elapsedTime;
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         tvElapsedTime = findViewById(R.id.tvElapsedTime);
         btnStartPause = findViewById(R.id.btnStartPause);
         btnReset = findViewById(R.id.btnReset);
+        btnSave = findViewById(R.id.btnSave);
 
         handler = new Handler();
         runnable = new Runnable() {
@@ -55,6 +63,18 @@ public class MainActivity extends AppCompatActivity {
                 resetTimer();
             }
         });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveElapsedTime();
+            }
+        });
+
+        // Recuperar o tempo salvo anteriormente, se houver
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        elapsedTime = sharedPreferences.getLong("elapsedTime", 0);
+        updateElapsedTime();
     }
 
     private void startTimer() {
@@ -63,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             isRunning = true;
             btnStartPause.setText("Pausar");
             btnReset.setVisibility(View.INVISIBLE);
+            btnSave.setVisibility(View.INVISIBLE);
             handler.postDelayed(runnable, 0);
         }
     }
@@ -72,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
             isRunning = false;
             btnStartPause.setText("Continuar");
             btnReset.setVisibility(View.VISIBLE);
+            btnSave.setVisibility(View.VISIBLE);
             pausedTime = elapsedTime; // Armazena o tempo decorrido atual
             handler.removeCallbacks(runnable);
         }
@@ -83,6 +105,28 @@ public class MainActivity extends AppCompatActivity {
         pausedTime = 0;
         updateElapsedTime();
         btnReset.setVisibility(View.INVISIBLE);
+        btnStartPause.setText("Iniciar");
+
+        // Limpar a lista de tempos salvos
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+
+    private void saveElapsedTime() {
+        if (!isRunning) {
+            // Salvar o tempo decorrido em SharedPreferences
+            SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            int count = sharedPreferences.getInt("count", 0);
+            editor.putString("time_" + count,  String.valueOf(tvElapsedTime.getText()));
+            editor.putInt("count", count + 1);
+            editor.apply();
+
+            // Exibir notificação
+            Toast.makeText(this, "Tempo salvo com sucesso!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void updateElapsedTime() {
@@ -94,6 +138,11 @@ public class MainActivity extends AppCompatActivity {
         int milliseconds = (int) (elapsedTime % 1000);
         String formattedTime = String.format("%02d:%02d:%03d", minutes, seconds, milliseconds);
         tvElapsedTime.setText(formattedTime);
+    }
+
+    public void onSavedTimesClicked(View view) {
+        Intent intent = new Intent(this, SavedTimesActivity.class);
+        startActivity(intent);
     }
 
     @Override
